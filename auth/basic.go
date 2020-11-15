751aa160288de454e01118d4e0a75ddf39bb5fa2
Lukasz Janyst <lukasz@jany.st>
2020-11-15 16:43:28 +0100

    auth: Limit the failed attempts to 3 per 5 minutes

--- auth/basic.go
+++ auth/basic.go
@@ -10,14 +10,21 @@ package auth
 import (
 	"fmt"
 	"math/rand"
+	"net"
 	"net/http"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+type attempt struct {
+	timestamp   time.Time
+	numAttemtps int
+}
+
 type BasicAuthHandler struct {
 	userMap        map[string]string
+	attemptMap     map[string]*attempt
 	realm          string
 	wrappedHandler http.Handler
 	rnd            *rand.Rand
@@ -32,8 +39,31 @@ func (handler BasicAuthHandler) writeUnauthorized(w http.ResponseWriter) {
 func (handler BasicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	sleepTime := handler.rnd.Intn(500000)
 
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		time.Sleep(time.Duration(sleepTime) * time.Microsecond)
+		handler.writeUnauthorized(w)
+		return
+	}
+
+	att, ok := handler.attemptMap[ip]
+	if !ok {
+		att = new(attempt)
+		handler.attemptMap[ip] = att
+	}
+
+	if att.numAttemtps >= 3 && att.timestamp.Add(5*time.Minute).After(time.Now()) {
+		att.numAttemtps = 0
+		att.timestamp = time.Now()
+		time.Sleep(time.Duration(sleepTime) * time.Microsecond)
+		handler.writeUnauthorized(w)
+		return
+	}
+
 	user, pass, ok := r.BasicAuth()
 	if !ok {
+		att.numAttemtps++
+		att.timestamp = time.Now()
 		time.Sleep(time.Duration(sleepTime) * time.Microsecond)
 		handler.writeUnauthorized(w)
 		return
@@ -41,17 +71,23 @@ func (handler BasicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 
 	knownPass, ok := handler.userMap[user]
 	if !ok {
+		att.numAttemtps++
+		att.timestamp = time.Now()
 		time.Sleep(time.Duration(sleepTime) * time.Microsecond)
 		handler.writeUnauthorized(w)
 		return
 	}
 
-	err := bcrypt.CompareHashAndPassword([]byte(knownPass), []byte(pass))
+	err = bcrypt.CompareHashAndPassword([]byte(knownPass), []byte(pass))
 	if err != nil {
+		att.numAttemtps++
+		att.timestamp = time.Now()
 		time.Sleep(time.Duration(sleepTime) * time.Microsecond)
 		handler.writeUnauthorized(w)
 		return
 	}
+
+	att.numAttemtps = 0
 	handler.wrappedHandler.ServeHTTP(w, r)
 }
 
@@ -60,6 +96,7 @@ func NewBasicAuthHandler(realm string, userMap map[string]string, handler http.H
 	h.realm = realm
 	h.wrappedHandler = handler
 	h.userMap = userMap
+	h.attemptMap = make(map[string]*attempt)
 	h.rnd = rand.New(rand.NewSource(99))
 	return h
 }
